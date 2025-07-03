1e89edbdc239a324027c3aa0b6bebeaa200b6ebd
Paweł Kubas <pawel.kubas.jan@gmail.com>
2025-07-03 18:25:42 +0200

    Init

---
+++ src/main/java/com/github/zabbum/pimanager/PimanagerApplication.java
@@ -0,0 +1,13 @@
+package com.github.zabbum.pimanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PimanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PimanagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/zabbum/pimanager/PimanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.zabbum.pimanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PimanagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
